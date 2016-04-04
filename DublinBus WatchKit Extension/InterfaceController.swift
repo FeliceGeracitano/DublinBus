4a3b40fff1356d6df59e83ee4fafd30a31a86b01
Felice Geracitano <felice.geracitano@gmail.com>
2016-04-04 01:05:51 +0100

    Init

---
+++ DublinBus WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  DublinBus WatchKit Extension
+//
+//  Created by Felice Geracitano on 04/04/2016.
+//  Copyright © 2016 Felice Geracitano. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ DublinBus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DublinBus
+//
+//  Created by Felice Geracitano on 04/04/2016.
+//  Copyright © 2016 Felice Geracitano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
